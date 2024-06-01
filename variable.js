67cd8b0ef50bf1e978df24c0fc90db4cf9596a7c
Dhea Marenty Suwarno <dheamarentys@gmail.com>
2024-06-02 01:31:22 +0700

    upload new file

---
+++ variable.js
@@ -0,0 +1,15 @@
+//const name = "Dhea";
+// name = "Eduwork";
+//console.log(name);
+
+//const student = {
+//    name:"Dhea",
+//    class: 07
+//}
+
+//student.faculty = "Informatics";
+//console.log(student);
+
+const students = ['Dhea', 'Jiha'];
+students.push('Sekar');
+console.log(students);
